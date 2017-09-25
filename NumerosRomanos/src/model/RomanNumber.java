3113379beaffde920035e6cd34de52654c609f45
Jose Carlos de Souza Filho <jcsouza@MacBook-Pro-3.local>
2017-09-25 16:39:31 -0300

    Simples refatorações

--- NumerosRomanos/src/model/RomanNumber.java
+++ NumerosRomanos/src/model/RomanNumber.java
@@ -3,12 +3,10 @@ package model;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class RomanNumber {
-
+class RomanNumber {
 		
-	private Map<Integer, String> numberToLetter = new LinkedHashMap<>();
-
-	public RomanNumber() {
+	private static Map<Integer, String> numberToLetter = new LinkedHashMap<>();
+	static {
 		numberToLetter.put(1000, "M");
 		numberToLetter.put(900, "CM");
 		numberToLetter.put(500, "D");
@@ -24,9 +22,8 @@ public class RomanNumber {
 		numberToLetter.put(1, "I");
 	}
 	
-	public int toArabic(String romano)  throws NumberFormatException {
-		if (romano.length() == 0)
-			throw new NumberFormatException("An empty string does not define a roman numeral.");
+	int toArabic(String romano)  throws NumberFormatException {
+		if (romano.length() == 0) throw new NumberFormatException("An empty string does not define a roman numeral.");
 
 		romano = romano.toUpperCase();
 
@@ -38,35 +35,28 @@ public class RomanNumber {
 			char letter = romano.charAt(i);
 			int number = letterToNumber(letter);  
 
-			if (number < 0) {
-				throw new NumberFormatException("Illegal character \"" + letter + "\" in roman numeral.");
-			}
+			if (number < 0) throw new NumberFormatException("Illegal character \"" + letter + "\" in roman numeral.");
 
 			i++; 
 
-			if (i == romano.length()) {               
-				arabic += number;
-			}
+			if (i == romano.length()) arabic += number;
 			else {
 				int nextNumber = letterToNumber(romano.charAt(i));
 				if (nextNumber > number) {
 					arabic += (nextNumber - number);
 					i++;
 				}
-				else {
-					arabic += number;
-				}
+				else arabic += number;
 			}
 
 		}
 		return arabic;
 	}
 
-	public String toRoman(int arabic) throws NumberFormatException {
-		if (arabic > 3999 || arabic < 1) {
-			throw new NumberFormatException("Invalid value.");
-		}
-		String roman = ""; 
+	String toRoman(int arabic) throws NumberFormatException {
+		if (arabic > 3999 || arabic < 1) throw new NumberFormatException("Invalid value.");
+
+		String roman = "";
 		for (Map.Entry<Integer, String> pair : numberToLetter.entrySet()) {
 			while(arabic >= pair.getKey()){
 				roman += pair.getValue();
@@ -78,14 +68,14 @@ public class RomanNumber {
 
 	private int letterToNumber(char letter) {
 		switch (letter) {
-		case 'I':  return 1;
-		case 'V':  return 5;
-		case 'X':  return 10;
-		case 'L':  return 50;
-		case 'C':  return 100;
-		case 'D':  return 500;
-		case 'M':  return 1000;
-		default:   return -1;
+			case 'I': return 1;
+			case 'V': return 5;
+			case 'X': return 10;
+			case 'L': return 50;
+			case 'C': return 100;
+			case 'D': return 500;
+			case 'M': return 1000;
+			default: return -1;
 		}
 	}
 }
